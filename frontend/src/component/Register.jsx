7d6d53bb70e0c46f6675c67905a1ff8639617213
SAUMYA SHARMA <sharmasaumya2464@gmail.com>
2025-07-15 16:48:32 +0530

    Update Register.jsx

--- frontend/src/component/Register.jsx
+++ frontend/src/component/Register.jsx
@@ -18,7 +18,7 @@ export default function Register() {
     setLoading(true);
     setError(null);
     try {
-      const res = await axios.post("	http://localhost:5000/api/auth/register", form);
+      const res = await axios.post("https://swapskill-backend1.onrender.com/api/auth/register", form);
       localStorage.setItem("token", res.data.token);
       navigate("/");
     } catch (err) {
